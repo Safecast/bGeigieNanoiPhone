dbd2ada1755895e531316fc6e7bcfdf087783f47
Yongping Chen <will@nowhere.co.jp>
2014-01-07 14:10:32 +0900

    Initial Commit

---
+++ bGeigieNanoiPhone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  bGeigieNanoiPhone
+//
+//  Created by Chen Yongping on 1/7/14.
+//  Copyright (c) 2014 Eyes, JAPAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ bGeigieNanoiPhoneTests/bGeigieNanoiPhoneTests.m
@@ -0,0 +1,34 @@
+//
+//  bGeigieNanoiPhoneTests.m
+//  bGeigieNanoiPhoneTests
+//
+//  Created by Chen Yongping on 1/7/14.
+//  Copyright (c) 2014 Eyes, JAPAN. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bGeigieNanoiPhoneTests : XCTestCase
+
+@end
+
+@implementation bGeigieNanoiPhoneTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
