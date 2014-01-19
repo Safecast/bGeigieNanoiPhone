b65436c23d118b5a59c8ad1a7ea2cd1ee4e67289
Yongping Chen <will@nowhere.co.jp>
2014-01-19 11:22:01 +0900

    add page view controller and single meter view controller

---
+++ bGeigieNanoiPhone/VCs/SingleMeterViewController.h
@@ -0,0 +1,13 @@
+//
+//  SingleMeterViewController.h
+//  bGeigieNanoiPhone
+//
+//  Created by Chen Yongping on 1/19/14.
+//  Copyright (c) 2014 Eyes, JAPAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingleMeterViewController : UIViewController
+
+@end
